769dc5519f11f54588850724c7e08248274e9adf
calvin.huynh <calvin.huynh@objectbrains.com>
2019-06-11 16:05:28 -0700

    updateing files to python3

--- cryptopals/challengeSet1/hex2Base64.py
+++ cryptopals/challengeSet1/hex2Base64.py
@@ -1,13 +1,17 @@
 import sys
-import codecs
+import base64
 
-var = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-print ("Challenge - Convert hex to base64")
 
-toBytes = bytes(var)
 
-encoded64P2 = toBytes.decode("hex").encode("base64")
-print "Python 2 encoding: " + encoded64P2
+def hexStr2Base64(hexstring):
+    decodedHexString = bytes.fromhex(hexstring)
+    decodedBase64 = base64.b64encode(decodedHexString)
+    return decodedBase64.decode()
 
-encoded64P3 = codecs.encode(codecs.decode(toBytes, 'hex'), 'base64').decode()
-print "Python 3 encoding: " + encoded64P3
+def main():
+    var = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    print ("Challenge - Convert hex to base64")
+    print(hexStr2Base64(var))
+
+if __name__ == '__main__':
+    main()
