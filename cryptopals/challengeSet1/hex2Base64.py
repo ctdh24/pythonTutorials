dcc9158e7d4621405f2436a910d0e8668b99bcfc
calvin.huynh <calvin.huynh@objectbrains.com>
2019-06-11 13:41:40 -0700

    reading from bytes instead of string

--- cryptopals/challengeSet1/hex2Base64.py
+++ cryptopals/challengeSet1/hex2Base64.py
@@ -1,11 +1,13 @@
 import sys
 import codecs
 
-var = sys.argv[1]
+var = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 print "Challenge - Convert hex to base64"
 
-encoded64P2 = var.decode("hex").encode("base64")
+toBytes = bytes(var)
+
+encoded64P2 = toBytes.decode("hex").encode("base64")
 print "Python 2 encoding: " + encoded64P2
 
-encoded64P3 = codecs.encode(codecs.decode(var, 'hex'), 'base64').decode()
+encoded64P3 = codecs.encode(codecs.decode(toBytes, 'hex'), 'base64').decode()
 print "Python 3 encoding: " + encoded64P3
